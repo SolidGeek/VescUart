0f14bd88d4626c5360ea6b0c8c9193ab5500e01a
Kevin Yacucci <kcy0511@gmail.com>
2024-03-05 17:07:49 -0500

    Make Indentation Consistent

--- src/VescUart.h
+++ src/VescUart.h
@@ -66,129 +66,129 @@ class VescUart
 		/** Variable to hold nunchuck values */
 		nunchuckPackage nunchuck; 
 
-       /** Variable to hold firmware version */
-        FWversionPackage fw_version; 
+    /** Variable to hold firmware version */
+    FWversionPackage fw_version; 
 
-        /**
-         * @brief      Set the serial port for uart communication
-         * @param      port  - Reference to Serial port (pointer) 
-         */
-        void setSerialPort(Stream* port);
+    /**
+     * @brief      Set the serial port for uart communication
+     * @param      port  - Reference to Serial port (pointer) 
+     */
+    void setSerialPort(Stream* port);
 
-        /**
-         * @brief      Set the serial port for debugging
-         * @param      port  - Reference to Serial port (pointer) 
-         */
-        void setDebugPort(Stream* port);
+    /**
+     * @brief      Set the serial port for debugging
+     * @param      port  - Reference to Serial port (pointer) 
+     */
+    void setDebugPort(Stream* port);
 
-        /**
-         * @brief      Populate the firmware version variables
-         *
-         * @return     True if successfull otherwise false
-         */
-        bool getFWversion(void);
+    /**
+     * @brief      Populate the firmware version variables
+     *
+     * @return     True if successfull otherwise false
+     */
+    bool getFWversion(void);
 
-        /**
-         * @brief      Populate the firmware version variables
-         *
-         * @param      canId  - The CAN ID of the VESC
-         * @return     True if successfull otherwise false
-         */
-        bool getFWversion(uint8_t canId);
+    /**
+     * @brief      Populate the firmware version variables
+     *
+     * @param      canId  - The CAN ID of the VESC
+     * @return     True if successfull otherwise false
+     */
+    bool getFWversion(uint8_t canId);
 
-        /**
-         * @brief      Sends a command to VESC and stores the returned data
-         *
-         * @return     True if successfull otherwise false
-         */
-        bool getVescValues(void);
+    /**
+     * @brief      Sends a command to VESC and stores the returned data
+     *
+     * @return     True if successfull otherwise false
+     */
+    bool getVescValues(void);
 
-        /**
-         * @brief      Sends a command to VESC and stores the returned data
-         * @param      canId  - The CAN ID of the VESC
-         *
-         * @return     True if successfull otherwise false
-         */
-        bool getVescValues(uint8_t canId);
+    /**
+     * @brief      Sends a command to VESC and stores the returned data
+     * @param      canId  - The CAN ID of the VESC
+     *
+     * @return     True if successfull otherwise false
+     */
+    bool getVescValues(uint8_t canId);
 
-        /**
-         * @brief      Sends values for joystick and buttons to the nunchuck app
-         */
-        void setNunchuckValues(void);
-        /**
-         * @brief      Sends values for joystick and buttons to the nunchuck app
-         * @param      canId  - The CAN ID of the VESC
-         */
-        void setNunchuckValues(uint8_t canId);
+    /**
+     * @brief      Sends values for joystick and buttons to the nunchuck app
+     */
+    void setNunchuckValues(void);
+    /**
+     * @brief      Sends values for joystick and buttons to the nunchuck app
+     * @param      canId  - The CAN ID of the VESC
+     */
+    void setNunchuckValues(uint8_t canId);
 
-        /**
-         * @brief      Set the current to drive the motor
-         * @param      current  - The current to apply
-         */
-        void setCurrent(float current);
+    /**
+     * @brief      Set the current to drive the motor
+     * @param      current  - The current to apply
+     */
+    void setCurrent(float current);
 
-        /**
-         * @brief      Set the current to drive the motor
-         * @param      current  - The current to apply
-         * @param      canId  - The CAN ID of the VESC
-         */
-        void setCurrent(float current, uint8_t canId);
+    /**
+     * @brief      Set the current to drive the motor
+     * @param      current  - The current to apply
+     * @param      canId  - The CAN ID of the VESC
+     */
+    void setCurrent(float current, uint8_t canId);
 
-        /**
-         * @brief      Set the current to brake the motor
-         * @param      brakeCurrent  - The current to apply
-         */
-        void setBrakeCurrent(float brakeCurrent);
+    /**
+     * @brief      Set the current to brake the motor
+     * @param      brakeCurrent  - The current to apply
+     */
+    void setBrakeCurrent(float brakeCurrent);
 
-        /**
-         * @brief      Set the current to brake the motor
-         * @param      brakeCurrent  - The current to apply
-         * @param      canId  - The CAN ID of the VESC
-         */
-        void setBrakeCurrent(float brakeCurrent, uint8_t canId);
+    /**
+     * @brief      Set the current to brake the motor
+     * @param      brakeCurrent  - The current to apply
+     * @param      canId  - The CAN ID of the VESC
+     */
+    void setBrakeCurrent(float brakeCurrent, uint8_t canId);
 
 
-        /**
-         * @brief      Set the rpm of the motor
-         * @param      rpm  - The desired RPM (actually eRPM = RPM * poles)
-         */
-        void setRPM(float rpm);		
+    /**
+     * @brief      Set the rpm of the motor
+     * @param      rpm  - The desired RPM (actually eRPM = RPM * poles)
+     */
+    void setRPM(float rpm);		
 
-        /**
-         * @brief      Set the rpm of the motor
-         * @param      rpm  - The desired RPM (actually eRPM = RPM * poles)
-         * @param      canId  - The CAN ID of the VESC
-         */
-        void setRPM(float rpm, uint8_t canId);
+    /**
+     * @brief      Set the rpm of the motor
+     * @param      rpm  - The desired RPM (actually eRPM = RPM * poles)
+     * @param      canId  - The CAN ID of the VESC
+     */
+    void setRPM(float rpm, uint8_t canId);
 
-        /**
-         * @brief      Set the duty of the motor
-         * @param      duty  - The desired duty (0.0-1.0)
-         */
-        void setDuty(float duty);
+    /**
+     * @brief      Set the duty of the motor
+     * @param      duty  - The desired duty (0.0-1.0)
+     */
+    void setDuty(float duty);
 
-        /**
-         * @brief      Set the duty of the motor
-         * @param      duty  - The desired duty (0.0-1.0)
-         * @param      canId  - The CAN ID of the VESC
-         */
-        void setDuty(float duty, uint8_t canId);
+    /**
+     * @brief      Set the duty of the motor
+     * @param      duty  - The desired duty (0.0-1.0)
+     * @param      canId  - The CAN ID of the VESC
+     */
+    void setDuty(float duty, uint8_t canId);
 
-        /**
-         * @brief      Send a keepalive message
-         */
-        void sendKeepalive(void);
+    /**
+     * @brief      Send a keepalive message
+     */
+    void sendKeepalive(void);
 
-        /**
-         * @brief      Send a keepalive message
-         * @param      canId  - The CAN ID of the VESC
-         */
-        void sendKeepalive(uint8_t canId);
+    /**
+     * @brief      Send a keepalive message
+     * @param      canId  - The CAN ID of the VESC
+     */
+    void sendKeepalive(uint8_t canId);
 
-        /**
-         * @brief      Help Function to print struct dataPackage over Serial for Debug
-         */
-        void printVescValues(void);
+    /**
+     * @brief      Help Function to print struct dataPackage over Serial for Debug
+     */
+    void printVescValues(void);
 
 	private: 
 
